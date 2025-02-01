1f8877671e5884d042bad67bf58c81573f98b02b
Longbow <zhangjian2080@outlook.com>
2025-02-01 22:55:13 +0800

    Add HelloSimpleApplication

---
+++ src/main/java/org/tutorial/App.java
@@ -0,0 +1,14 @@
+package org.tutorial;
+
+import org.tutorial.sample.HelloSimpleApplication;
+
+import com.jme3.app.SimpleApplication;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        SimpleApplication app = new HelloSimpleApplication();
+        app.start();
+    }
+}
